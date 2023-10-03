950c4195ec0278cf7ff85213acdaeafa3188ba90
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-03 22:45:36 +0100

    Data service support

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,16 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
   constructor(private http: HttpClient) {}
 
-  collectData(query: string, filter?: string): Observable<any> {
-    let url = `dbh/data_collector.php?query=${query}`;
+  collectData(query: string, filter?: string): Observable<any[]> {
+    let url = `http://localhost:8080/dbh/data_collector.php?query=${query}`;
     if (filter != null) {
       url += `&filter=${encodeURIComponent(filter)}`;
     }
-    return this.http.get(url);
+    return this.http.get<any[]>(url);
   }
 }
