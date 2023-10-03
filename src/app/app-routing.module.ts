ef60a501fe8deaa5fdac13e630c24fd8213d85af
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-03 22:44:38 +0100

    Made home default route, added products route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { CategoryViewComponent } from './components/category-view/category-view.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
-  // redirect to home by default
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'products/:category', component: CategoryViewComponent }
 ];
 
 @NgModule({
