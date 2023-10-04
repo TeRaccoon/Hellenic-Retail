8cae0a8daa487895428a757d77f157641aace4ea
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-04 22:35:11 +0100

    Font awesome support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
 import { CategoryViewComponent } from './components/category-view/category-view.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,14 @@ import { CategoryViewComponent } from './components/category-view/category-view.
     HomeComponent,
     ContactComponent,
     SlideshowComponent,
-    CategoryViewComponent
+    CategoryViewComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
