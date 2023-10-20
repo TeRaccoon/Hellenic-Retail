1c8b049ea2e1730f485f0b3b4156cebb47e64675
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-21 00:17:03 +0100

    Changed "view" for "shop"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { CategoryViewComponent } from './components/category-view/category-view.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'products/:category', component: CategoryViewComponent }
+  { path: 'shop/:category', component: ShopComponent }
 ];
 
 @NgModule({
