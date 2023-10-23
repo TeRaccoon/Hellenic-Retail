18011f95c93d9a3f3c6b32475cf2c4097dd8c52d
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-23 23:54:37 +0100

    Shop page development

--- src/app/components/shop-grid/shop-grid.component.ts
+++ src/app/components/shop-grid/shop-grid.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
 
 @Component({
@@ -10,19 +10,29 @@ import { DataService } from '../../services/data.service';
 export class ShopGridComponent {
   resultsAmount: any;
   products: any[] = [];
+  oldPrices: any[] = [];
 
-  constructor(private dataService: DataService, private router: Router) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) { }
   
   ngOnInit() {
-    let url = this.router.url;
-    let urlArray = url.split('/');
-    let lastSegment = decodeURI(urlArray[urlArray.length - 1]);
-    this.loadProducts(lastSegment);
+    this.route.params.subscribe(params => {
+      const category = params['category'];
+      this.loadProducts(category);
+    });
   }
 
-  async loadProducts(lastSegment: string) {
-    this.dataService.collectData("products-from-category", lastSegment).subscribe((data: any) => {
+  async loadProducts(category:string) {
+    this.dataService.collectData("products-from-category", category).subscribe((data: any) => {
       this.products = data;
+      this.resultsAmount = data.length === undefined ? 1 : data.length;
+      data.forEach((product: any) => {
+        if (product.discount && product.discount != null) {
+          this.oldPrices.push(product.price * ((100 - product.discount) / 100));
+        } else {
+          this.oldPrices.push(null);
+        }
+      });
+      console.log(this.oldPrices);
     });
   }
 }
