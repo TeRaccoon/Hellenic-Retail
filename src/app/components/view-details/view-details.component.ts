bd1b4e5a1cfce5156593adc89738437cb67f6cec
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-27 18:00:19 +0100

    Share option development

--- src/app/components/view-details/view-details.component.ts
+++ src/app/components/view-details/view-details.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faHeart, faEye, faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-view-details',
@@ -13,12 +15,15 @@ export class ViewDetailsComponent {
   faHeart = faHeart;
   faEye = faEye;
   faClipboard = faClipboard;
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
   
   product: any;
   fullPath: string = '';
   oldPrice: (number | null) = null;
 
-  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private clipboard: Clipboard) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private clipboard: Clipboard, private location: Location) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -31,7 +36,7 @@ export class ViewDetailsComponent {
   share(option: string) {
     switch(option) {
       case 'clipboard':
-        this.clipboard.copy(textToCopy);
+        this.clipboard.copy("https://hellenicgrocery.co.uk/" + this.location.path());
         break;
     }
   }
