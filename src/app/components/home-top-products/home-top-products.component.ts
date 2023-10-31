8f06862fd3860ec120e277e775c1efdc6326ca5b
TeRaccoon <jack.sam.benning@gmail.com>
2023-10-31 16:47:42 +0000

    Animation fixes

--- src/app/components/home-top-products/home-top-products.component.ts
+++ src/app/components/home-top-products/home-top-products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { faHeart, faEye } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-top-products',
@@ -7,7 +8,11 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./home-top-products.component.scss']
 })
 export class HomeTopProductsComponent {
+  faHeart = faHeart;
+  faEye = faEye;
+
   topProducts: any[] = [];
+  oldPrices: (number | null)[] = [];
 
   constructor(private dataService: DataService) { }
 
@@ -18,20 +23,18 @@ export class HomeTopProductsComponent {
   async loadProducts() {
     this.dataService.collectData('top-products', '10').subscribe((data: any) => {
       this.topProducts = data;
-      this.calculateOffer();
+      this.oldPrices = this.topProducts.map((product: any) => {
+        if (product.discount && product.discount != null) {
+          console.log(product.discount);
+          return product.price * ((100 - product.discount) / 100);
+        } else {
+          return null;
+        }
+      });
     });
   }
 
-  calculateOffer() {
-    this.topProducts.forEach(product => {
-      if (product.offer_id != null) {
-        console.log(product.offer_id);
-        switch (product.offer_id) {
-          case 1:
-            product.offer_id = product.price * 0.95;
-            break;
-        }
-      }
-    })
+  openImage(imageLocation: string) {
+    window.open('assets/uploads/admin_uploads/' + imageLocation, '_blank');
   }
 }
