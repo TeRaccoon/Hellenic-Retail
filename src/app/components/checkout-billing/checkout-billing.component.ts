775ecef753ff7872c7e69c431b07822ad0bdb717
TeRaccoon <jack.sam.benning@gmail.com>
2023-12-10 22:11:26 +0000

    General development

--- src/app/components/checkout-billing/checkout-billing.component.ts
+++ src/app/components/checkout-billing/checkout-billing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-billing',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-billing.component.scss']
 })
 export class CheckoutBillingComponent {
+  billingForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.billingForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      companyName: [''],
+      streetAddress1: ['', Validators.required],
+      streetAddress2: [''],
+      town: ['', Validators.required],
+      county: ['', Validators.required],
+      postcode: ['', Validators.required],
+      phone: ['', Validators.required, Validators.minLength(7), Validators.maxLength(14)],
+      email: ['', [Validators.required, Validators.email]],
+      orderNotes: ['']
+    });
+  }
 }
