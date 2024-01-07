565f0d15b347e347aec59107ea99499964cf8230
TeRaccoon <jack.sam.benning@gmail.com>
2024-01-07 13:22:50 +0000

    Feature: Added rudementary coupon validation

--- src/app/components/checkout-coupon/checkout-coupon.component.ts
+++ src/app/components/checkout-coupon/checkout-coupon.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-checkout-coupon',
@@ -7,6 +9,26 @@ import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./checkout-coupon.component.scss']
 })
 export class CheckoutCouponComponent {
+  couponForm: FormGroup;
   faInfoCircle = faInfoCircle;
   shown = false;
+
+  constructor(private dataService: DataService, private fb: FormBuilder) {
+    this.couponForm = this.fb.group({
+      "code": ['', Validators.required]
+    });
+  }
+
+  formSubmit() {
+    if (this.couponForm.valid) {
+      const formData = this.couponForm.value;
+      this.dataService.submitFormData('coupon', formData).subscribe((data: any) => {
+        if (data != null) {
+          console.log("Coupon valid");
+        } else {
+          console.log("Coupon invalid");
+        }
+      })
+    }
+  }
 }
