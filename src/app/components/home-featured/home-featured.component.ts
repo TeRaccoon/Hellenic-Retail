db36db5f8dcdc78e7b64c8c4dfd522bfe65b12b5
TeRaccoon <jack.sam.benning@gmail.com>
2024-03-29 23:11:59 +0000

    Refactor: Overhaul

--- src/app/components/home-featured/home-featured.component.ts
+++ src/app/components/home-featured/home-featured.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { faHeart, faEye } from '@fortawesome/free-solid-svg-icons';
+import { FormService } from 'src/app/services/form.service';
+import { CartService } from 'src/app/services/cart.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home-featured',
@@ -7,13 +11,16 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./home-featured.component.scss']
 })
 export class HomeFeaturedComponent {
+  faHeart = faHeart;
+  faEye = faEye;
+
   featuredProducts: any[] = [];
   oldPrices: (number | null)[] = [];
   featuredData: any;
 
   imageUrl = '';
 
-  constructor(private dataService: DataService) { }
+  constructor(private cartService: CartService, private dataService: DataService, private formService: FormService) { }
 
   ngOnInit() {
     this.loadOffers();
@@ -21,19 +28,26 @@ export class HomeFeaturedComponent {
   }
 
   async loadOffers() {
-    this.dataService.collectData('featured', '3').subscribe((data: any) => {
-      this.featuredProducts = data;
-      this.oldPrices = this.featuredProducts.map((product: any) => {
-        if (product.discount && product.discount != null) {
-          console.log(product.discount);
-          return product.price * ((100 - product.discount) / 100);
-        } else {
-          return null;
-        }
-      });
+    let featuredProducts = await lastValueFrom(this.dataService.collectData('featured', '3'));
+    this.featuredProducts = Array.isArray(featuredProducts) ? featuredProducts : [featuredProducts];
+
+    this.oldPrices = this.featuredProducts.map((product: any) => {
+      if (product.discount && product.discount != null) {
+        return product.price * ((100 - product.discount) / 100);
+      } else {
+        return null;
+      }
     });
-    this.dataService.collectData('section-data', 'home-featured').subscribe((data: any) => {
-      this.featuredData = data;
-    });
-  } 
+
+    this.featuredData = await lastValueFrom(this.dataService.collectData('section-data', 'home-featured'));
+  }
+
+  openImage(imageLocation: string) {
+    window.open(this.imageUrl + imageLocation, '_blank');
+  }
+
+  async addToCart(productID: number, quantity: number) {
+    this.cartService.addToCart(productID, quantity);
+    this.formService.showCartForm();
+  }
 }
