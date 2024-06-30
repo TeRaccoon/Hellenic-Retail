30e0c1538a4cc2a4f2d012b4a56232bc2f7b1b6f
xlea-dasm <harris-alex@outlook.com>
2024-06-30 16:31:48 +0100

    wishlist ect put in sidebar for mobile, set up click function for sidebar on mobile

--- src/app/components/navbar-category-search/navbar-category-search.component.ts
+++ src/app/components/navbar-category-search/navbar-category-search.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCartShopping, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { lastValueFrom } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormService } from 'src/app/services/form.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-category-search',
@@ -9,10 +13,18 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavbarCategorySearchComponent {
   faBars = faBars;
+  faUser = faUser;
+  faHeart = faHeart;
+  faCartShopping = faCartShopping;
+  
   categories: string[] = [];
   categoriesShown = false;
 
-  constructor(private dataService: DataService) { }
+  loginVisible = 'hidden';
+  cartVisible = 'hidden';
+  cartState: string = 'inactive';
+
+  constructor(private dataService: DataService, private authService: AuthService, private formService: FormService, private router: Router) { }
 
   ngOnInit() {
     this.loadCategories();
@@ -26,5 +38,21 @@ export class NavbarCategorySearchComponent {
 
   toggleCategory() {
     this.categoriesShown = !this.categoriesShown;
+  }
+  
+  async showAccount() {
+    let loginResponse = await lastValueFrom(this.authService.checkLogin());
+    if (loginResponse.success) {
+      this.router.navigate(['/account']);
+    } else {
+      this.toggleLogin();
+    }
+  }
+
+  toggleLogin() {
+    this.loginVisible == 'hidden' && this.formService.showLoginForm(); 
+  }
+  toggleCart() {
+    this.cartVisible == 'hidden' && this.formService.showCartForm();
   }
 }
