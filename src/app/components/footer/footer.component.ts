62635b30a459cb4760f696142410bef306ccf601
xlea-dasm <harris-alex@outlook.com>
2024-07-18 23:25:47 +0100

    adds 77px to the bottom of footer to create space for mobile navbar

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { DataService } from 'src/app/services/data.service';
 import { FormService } from 'src/app/services/form.service';
 import { faHouse, faPhone, faEnvelope, faFileInvoice, faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { lastValueFrom } from 'rxjs';
+import { RenderService } from 'src/app/services/render.service';
 
 
 @Component({
@@ -20,11 +21,16 @@ export class FooterComponent {
   imageUrl = '';
   categories: any[] = [];
 
-  constructor(private dataService: DataService, private formService: FormService) { }
+  screenSize: any = {};
+
+  constructor(private dataService: DataService, private renderService: RenderService) { }
 
   ngOnInit() {
     this.imageUrl = this.dataService.getUploadURL();
     this.loadCategories();
+    this.renderService.getScreenSize().subscribe(size => {
+      this.screenSize = size;
+    });
   }
 
   async loadCategories() {
