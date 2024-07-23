979e181235dc6f01bd1f45a9af814a0d5b63bb06
TeRaccoon <jack.sam.benning@gmail.com>
2024-07-24 00:10:43 +0100

    Feature: Added comprehensive checkout service

---
+++ src/app/common/types/checkout.ts
@@ -0,0 +1,6 @@
+export interface CheckoutSummary {
+    subtotal: number,
+    delivery: number,
+    vat: number,
+    total: number
+}
---
+++ src/app/services/checkout.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { CheckoutSummary } from '../common/types/checkout';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CheckoutService {
+    private checkoutSummarySubject: BehaviorSubject<CheckoutSummary>;
+
+    constructor() {
+        const initialSummary: CheckoutSummary = {
+            subtotal: 0,
+            delivery: 0,
+            vat: 0,
+            total: 0
+        };
+
+        this.checkoutSummarySubject = new BehaviorSubject<CheckoutSummary>(initialSummary);
+    }
+
+    public setCheckoutSummary(checkoutSummary: CheckoutSummary): void {
+        this.checkoutSummarySubject.next(checkoutSummary);
+    }
+
+    public updateCheckoutSummary(partialSummary: Partial<CheckoutSummary>): void {
+        const currentSummary = this.checkoutSummarySubject.value;
+        const updatedSummary = {
+            ...currentSummary,
+            ...partialSummary
+        };
+        this.checkoutSummarySubject.next(updatedSummary);
+    }
+
+    public getCheckoutSummary(): CheckoutSummary {
+        return this.checkoutSummarySubject.value;
+    }
+
+    public getCheckoutSummaryObservable(): Observable<CheckoutSummary> {
+        return this.checkoutSummarySubject.asObservable();
+    }
+}
