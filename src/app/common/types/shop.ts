da40d76ddfa9f74cc733c18a7b2dfba4709ca418
TeRaccoon <jack.sam.benning@gmail.com>
2024-08-25 12:10:55 +0100

    Feature: Added strict type for product details

--- src/app/common/types/shop.ts
+++ src/app/common/types/shop.ts
@@ -7,3 +7,21 @@ export interface Product {
     discount: number,
     image_location: string
 };
+
+export interface ProductDetails {
+    id: number,
+    category: string,
+    sub_category: string,
+    description: string,
+    brand: string,
+    discount: number,
+    name: string,
+    price: number,
+    discounted_price: number,
+    discounted_box_price?: number,
+    discounted_pallet_price?: number,
+    box_price?: number,
+    pallet_price?: number,
+    box_size?: number,
+    pallet_size?: number
+}
