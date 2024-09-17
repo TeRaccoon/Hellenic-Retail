59d7cc57a503c2e7acb8aa46e0a1ce80a777d9c7
TeRaccoon <jack.sam.benning@gmail.com>
2024-09-17 19:56:44 +0100

    HELL-175: Partly implemented box price for retail customers view

--- src/app/common/types/shop.ts
+++ src/app/common/types/shop.ts
@@ -1,27 +1,28 @@
 export interface Product {
-    name: string,
-    retail_price: number,
-    wholesale_price: number,
-    box_price: number,
-    pallet_price: number,
-    discount: number,
-    image_location: string
-};
+  name: string;
+  retail_price: number;
+  wholesale_price: number;
+  box_price: number;
+  pallet_price: number;
+  discount: number;
+  image_location: string;
+}
 
 export interface ProductDetails {
-    id: number,
-    category: string,
-    sub_category: string,
-    description: string,
-    brand: string,
-    discount: number,
-    name: string,
-    price: number,
-    discounted_price: number,
-    discounted_box_price?: number,
-    discounted_pallet_price?: number,
-    box_price?: number,
-    pallet_price?: number,
-    box_size?: number,
-    pallet_size?: number
+  id: number;
+  category: string;
+  sub_category: string;
+  description: string;
+  brand: string;
+  discount: number;
+  name: string;
+  price: number;
+  discounted_price: number;
+  discounted_box_price?: number;
+  discounted_pallet_price?: number;
+  box_price?: number;
+  retail_box_price?: number;
+  pallet_price?: number;
+  box_size?: number;
+  pallet_size?: number;
 }
