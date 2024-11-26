5b023fa24639e7d724729d4600d2702b16d54053
TeRaccoon <jack.sam.benning@gmail.com>
2024-11-26 18:48:48 +0000

    HELL-306: Added logging

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -10,18 +10,27 @@ export class ConfigService {
   private config: any;
   private loaded = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // this.loadConfig();
+  }
 
   async loadConfig() {
     this.config = await lastValueFrom(this.http.get(environment.configUrl));
     this.loaded = true;
+    console.log('config loaded');
   }
 
   async getConfig() {
     if (!this.loaded) {
+      console.log('waiting');
       await this.loadConfig();
     }
+    console.log('done waiting');
 
     return this.config;
+  }
+
+  isLoaded() {
+    return this.loaded;
   }
 }
--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -19,6 +19,7 @@ export class UrlService {
   }
 
   async loadConfig() {
+    console.log('url receive request');
     this.HOST_NAME = (await this.config.getConfig()).host;
   }
 
