e46b7aeb8cbd3c5a59252809c48ede9cc060455e
TeRaccoon <jack.sam.benning@gmail.com>
2024-11-27 22:47:30 +0000

    HELL-312: Fixed category overflow on home page

--- src/app/components/home-category-row/home-category-row.component.ts
+++ src/app/components/home-category-row/home-category-row.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { UrlService } from 'src/app/services/url.service'
+import { UrlService } from 'src/app/services/url.service';
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-category-row',
   templateUrl: './home-category-row.component.html',
-  styleUrls: ['./home-category-row.component.scss']
+  styleUrls: ['./home-category-row.component.scss'],
 })
 export class HomeCategoryRowComponent {
   categories: any[] = [];
   imageUrl = '';
 
-  constructor(private urlService: UrlService, private dataService: DataService) { }
+  caretRight = faCaretRight;
+  caretLeft = faCaretLeft;
+
+  constructor(
+    private urlService: UrlService,
+    private dataService: DataService
+  ) {}
 
   ngOnInit() {
-    this.imageUrl = this.urlService.getUrl('uploads');;
+    this.imageUrl = this.urlService.getUrl('uploads');
     this.getCategories();
   }
 
   async getCategories() {
-    let categories = await this.dataService.processGet("visible-categories");
+    let categories = await this.dataService.processGet('visible-categories');
     if (categories != null) {
       this.categories = categories;
     }
@@ -28,5 +35,15 @@ export class HomeCategoryRowComponent {
   onImageError(event: Event) {
     const target = event.target as HTMLImageElement;
     target.src = this.imageUrl + 'placeholder.jpg';
+  }
+
+  scroll(direction: 'left' | 'right'): void {
+    const categoryRow = document.querySelector('.category-row') as HTMLElement;
+    const scrollAmount = 200;
+    if (direction === 'left') {
+      categoryRow.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else if (direction === 'right') {
+      categoryRow.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
   }
 }
