a6ac7f6b72968b989517cc5c37048c764d96a82d
TeRaccoon <jack.sam.benning@gmail.com>
2024-12-10 21:26:07 +0000

    HELL-173: Removed unimplemented edit icons and added styling to make order history and address book mobile friendly

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -180,6 +180,7 @@ export class AccountComponent {
         'There was an error adding this address!',
         true
       );
+      this.pendingDeleteId = null;
     } else {
       this.formService.setPopupMessage('Address added successfully', true);
       this.addAddressForm.reset();
