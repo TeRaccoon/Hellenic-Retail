7948416f875f51e53563606e66cc3041ef47c80b
TeRaccoon <jack.sam.benning@gmail.com>
2025-01-12 11:12:43 +0000

    Refactor: Removed residual console logs

--- src/app/components/shop-grid/shop-grid.component.ts
+++ src/app/components/shop-grid/shop-grid.component.ts
@@ -260,7 +260,6 @@ export class ShopGridComponent {
   }
 
   isInPriceRange(product: any) {
-    console.log(product);
     const priceToCheck =
       product.discounted_price != null
         ? product.discounted_price
